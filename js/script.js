52f5072f5ca1339e35efe1dcc07a7712aaabe966
varFox <31412438+varFox@users.noreply.github.com>
2019-05-13 11:18:30 +0300

    12

--- js/script.js
+++ js/script.js
@@ -1,36 +1,26 @@
 let inputRub = document.getElementById('rub'),
-    inputUsd = document.getElementById('usd');
+  inputUsd = document.getElementById('usd');
 
-function getUsd(rub) {
-    return new Promise(function(resolve, reject) {
-        rub.addEventListener('input', () => {
-            let request = new XMLHttpRequest();
-    
-            request.open('GET', 'js/current.json');
-            request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
-            
-            
-            request.addEventListener('readystatechange', function() {
-                if (request.readyState === 4 && request.status == 200) {
-                  resolve();
-                } else {
-                  reject();
-                }
-            });
-            request.send();
-        });  
-    })
-      
+function getUsd() {
+  return new Promise(function (resolve, reject) {
+    let request = new XMLHttpRequest();
+    request.open('GET', 'js/current.json');
+    request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+    request.send();
+    request.addEventListener('readystatechange', function () {
+      if (request.readyState === 4 && request.status == 200) {
+        let data = JSON.parse(request.response);
+        resolve(data.usd);
+      } else if (request.status != 200) {
+        reject();
+      }
+    });
+  })
+
 }
-getUsd(inputRub)
-    .then(() => {
-      let data = JSON.parse(request.response);
-      inputUsd.value = rub.value / data.usd;
-    })
+
+inputRub.addEventListener('input', () => {
+  getUsd()
+    .then(usd => inputUsd.value = inputRub.value / usd)
     .catch(() => inputUsd.value = "Что-то пошло не так!");
-
-
-
-
-
-
+});
